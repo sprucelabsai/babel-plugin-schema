aabe7931b147eba7776dee209f0bfefa9a5bb2c3
Ryan Johnson <ryan@sprucelabs.ai>
2020-12-30 11:21:35 -0500

    lint

--- src/__tests__/behavioral/SchemaBuildsAndMapsPaths.test.ts
+++ src/__tests__/behavioral/SchemaBuildsAndMapsPaths.test.ts
@@ -61,7 +61,7 @@ export default class SchemaBuildsAndMapsPathsTest extends AbstractSpruceTest {
 			)
 			const promise2 = srcExists
 				? this.resolvePathAliasesUsingCommandLine(srcPath, resolveOptions)
-				: Promise.resolve()
+				: Promise.resolve(undefined)
 
 			await Promise.all([promise1, promise2])
 		} else {
